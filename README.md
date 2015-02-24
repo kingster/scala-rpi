1c748d14356a253db275f34718e9b984fb44ec67
Kinshuk Bairagi <me@kinshuk.in>
2015-02-25 00:20:31 +0530

    initial project created

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# scala-pi
+# scala-rpi
 Scala with Raspberry PI2
+
+#Building
+
+```bash
+$ sbt
+> compile
+> run
+```
+
+#Run
+
+- To Run Locally `sbt run`
+- To build Jar and then run
+    ```bash
+    $ sbt assembly
+    $ java -jar target/scala-2.10/scala-rpi.jar
+    ```
+
+- To Run on Raspberry PI `./scripts/run-rpi.sh -h "pi@hostname"`
+
---
+++ scripts/run-rpi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -x #debug mode
+
+echo "Starting Remote Run.."
+
+
+function die() {
+    echo "ERROR: $1" >&2
+    echo "Aborting." >&2
+    exit 1
+}
+
+
+HOST=
+MAIN_CLASS=
+
+while getopts h:c: opt; do
+  case $opt in
+  h)
+      HOST=$OPTARG
+      ;;
+  c)
+      MAIN_CLASS=$OPTARG
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [ -z "$HOST" ]; then
+   die "Host Cannot Be Empty"
+fi
+
+echo "Starting Build.."
+
+sbt assembly
+[ $? -ne 0 ] && die "Build failure"
+
+
+
+scp target/scala-2.10/scala-rpi.jar $HOST:./scala-rpi.jar
+ssh $HOST 'java -Xss8M -Xms256M -jar scala-rpi.jar'
